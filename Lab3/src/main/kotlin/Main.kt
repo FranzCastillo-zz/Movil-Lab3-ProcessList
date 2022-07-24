0487b6e09bbddf8abe48adeab1ce89ded535ed05
Francisco Castillo <cas21562@uvg.edu.gt>
2022-07-23 23:54:32 -0600

    Code Efficiency Improved

--- Lab3/src/main/kotlin/Main.kt
+++ Lab3/src/main/kotlin/Main.kt
@@ -16,77 +16,19 @@ fun main() {
 // CHECKS EVERY ITEM IN THE inputList PARAMETER AND RETURNS A LIST WITH INFORMATION OF
 // EACH OBJECT
 fun processList(inputList: List<Any?>?): List<ItemData>? {
-    if(inputList != null){ //IF THE PARAMETER ISN'T NULL
-        var returnList: MutableList<ItemData> = mutableListOf<ItemData>() // LIST TO RETURN
-        var i: Int = 0 // TO STORE THE originalPos
-
-        inputList.forEach{
-            if(it != null){
-                when (it) {
-                    is String 	-> returnList.add(isString(it, i))
-                    is Int 		-> returnList.add(isInt(it, i))
-                    is Boolean 	-> returnList.add(isBool(it, i))
-                    else 		-> returnList.add(isOther(it, i))
-                }
+    if (inputList == null) { return null }
+    val returnList: MutableList<ItemData> = mutableListOf<ItemData>() // LIST TO RETURN
+    var i = 0 // TO STORE THE originalPos
+    inputList.forEach {
+        if (it != null) returnList.add(
+            when (it) {
+                is String -> ItemData(i, it, "cadena", "l" + it.length)
+                is Int -> ItemData(i, it, "entero", if (it % 10 == 0) "M10" else if (it % 5 == 0) "M5" else if (it % 2 == 0) "M2" else null)
+                is Boolean -> ItemData(i, it, "booleano", if (it) "Verdadero" else "Falso")
+                else -> ItemData(i, it, null, null)
             }
-            i++
-        }
-
-        return returnList
-    }else{
-        return null
+        )
+        i++
     }
-}
-
-fun isString(item: String, pos: Int): ItemData{
-    return ItemData(
-        originalPos = pos,
-        originalValue = item,
-        type = "cadena",
-        info = "l" + item.length
-    )
-}
-
-fun isInt(item: Int, pos: Int): ItemData{
-    var infoMod: String? = null
-
-    if(item % 10 == 0){
-        infoMod = "M10"
-    }else if(item % 5 == 0){
-        infoMod = "M5"
-    }else if(item % 2 == 0){
-        infoMod = "M2"
-    }
-
-    return ItemData(
-        originalPos = pos,
-        originalValue = item,
-        type = "entero",
-        info = infoMod
-    )
-}
-
-fun isBool(item: Boolean, pos: Int): ItemData{
-    var infoValue: String
-    if(item){
-        infoValue = "Verdadero"
-    }else{
-        infoValue = "Falso"
-    }
-
-    return ItemData(
-        originalPos = pos,
-        originalValue = item,
-        type = "booleano",
-        info = infoValue
-    )
-}
-
-fun isOther(item: Any, pos: Int): ItemData{
-    return ItemData(
-        originalPos = pos,
-        originalValue = item,
-        type = null,
-        info = null
-    )
+    return returnList
 }
